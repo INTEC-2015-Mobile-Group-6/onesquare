f44fe6ccaa989661a3dd5d1d601dcf033c22715a
Francis Brito <fr.br94@gmail.com>
2015-03-11 12:03:36 -0400

    Add image optimization gulp task.

--- src/web/gulpfile.js
+++ src/web/gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-    vulcanize = require('gulp-vulcanize');
+    vulcanize = require('gulp-vulcanize'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant');
 
 gulp.task('default', ['bundle']);
 
-gulp.task('bundle', function () {
+gulp.task('bundle', ['optimize-images'], function () {
     return gulp.src('./public/index.html')
         .pipe(vulcanize({
             dest: './dist',
@@ -11,4 +13,12 @@ gulp.task('bundle', function () {
             abspath: './public'
         }))
         .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('optimize-images', function () {
+    return gulp.src('./public/images/*.png')
+        .pipe(imagemin({
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./dist/images'));
 });
