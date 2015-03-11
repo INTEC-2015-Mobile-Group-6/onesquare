0df250f232c388ed0228061ae2637e2678d3c9a1
Francis Brito <fr.br94@gmail.com>
2015-03-11 11:15:09 -0400

    Add bundle gulp task.

--- src/web/gulpfile.js
+++ src/web/gulpfile.js
@@ -1,5 +1,15 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    vulcanize = require('gulp-vulcanize');
 
 gulp.task('default', function () {
     console.log('it works!');
+});
+
+gulp.task('bundle', function () {
+    return gulp.src('./public/index.html')
+        .pipe(vulcanize({
+            dest: './dist',
+            strip: true
+        }))
+        .pipe(gulp.dest('./dist'));
 });
