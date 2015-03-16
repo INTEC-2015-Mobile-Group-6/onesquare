30d0de68ad7e4b1e9c4c61cfc485c48848a4092a
Francis Brito <fr.br94@gmail.com>
2015-03-16 12:37:06 -0400

    Add build Gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,32 @@
 'use strict';
 
-var gulp = require('gulp'),
-    gutil = require('gulp-util');
+require('dotenv').load();
 
-gulp.task('default', function () {
-    gutil.log('it works!');
+var pkg = require('./package.json'),
+    path = require('path');
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    plumber = require('gulp-plumber'),
+    pgbuild = require('gulp-phonegap-build');
+
+var dir = {
+    out: path.join(__dirname, './out'),
+    dist: path.join(__dirname, './dist'),
+    assets: path.join(__dirname, './public'),
+};
+
+
+gulp.task('build', function () {
+    gulp.src(dir.assets + '/**')
+        .pipe(plumber())
+        .pipe(pgbuild({
+            appId: process.env.PG_BUILD_APP_ID,
+            download: {
+                android: dir.out + '/onesquare-' + pkg.version + '.apk'
+            },
+            user: {
+                token: process.env.PG_BUILD_AUTH_TOKEN
+            }
+        }));
 });
